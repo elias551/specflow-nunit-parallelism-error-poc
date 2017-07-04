2c6b844f2550b96630b6b5162dd58373364b952f
Elias Platek <elias.platek@availpro.com>
2017-07-04 17:44:50 +0200

    first commit

---
+++ ClassLibrary1/ClassLibrary1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+        }
+    }
+}
